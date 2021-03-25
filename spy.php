9cff5cfb716dc287296cfb6e5c324d77ad3ff7ca
zloileshii <zloileshii@gmail.com>
2021-03-25 11:26:05 +0700

    Добавление пользователя с другого сайта + модификация интерфейса

--- spy.php
+++ spy.php
@@ -10,21 +10,39 @@
 <body>
 
     <?php
-    $pdo = new PDO('mysql:host=?;dbname=?;charset=utf8', '?', '?');
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=?;charset=utf8', '?', '?');
     $res = $pdo->query('SELECT * FROM node_revisions WHERE uid = 227 ORDER BY timestamp');
     $res = $res->fetchAll(PDO::FETCH_ASSOC);
     ?>
 
+    <h3>English</h3>
     <ol>
         <?php
         foreach ($res as $k) {
             // echo '<pre>';
             // print_r($k);
             // echo '</pre>';
-            echo "<li><small>" . date('Y-m-d H:i:s', $k['timestamp']) . "</small> <a href='http://www.sfu-kras.ru/node/{$k['nid']}'>Darya Sharkevich</a></li>";
+            echo "<li><small>" . date('Y-m-d H:i:s', $k['timestamp']) . "</small> <strong>Darya Sharkevich</strong> <a href='http://www.sfu-kras.ru/node/{$k['nid']}'>http://www.sfu-kras.ru/node/{$k['nid']}</a></li>";
         }
         ?>
+    </ol>
 
+    <?php
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=?;charset=utf8', '?', '?');
+    $res = $pdo->query('SELECT * FROM node_revisions WHERE uid = 24 ORDER BY timestamp');
+    $res = $res->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+
+    <h3>Sport</h3>
+    <ol>
+        <?php
+        foreach ($res as $k) {
+            // echo '<pre>';
+            // print_r($k);
+            // echo '</pre>';
+            echo "<li><small>" . date('Y-m-d H:i:s', $k['timestamp']) . "</small> <strong>Irina Verhoturova</strong> <a href='http://www.sport.sfu-kras.ru/node/{$k['nid']}'>http://www.sport.sfu-kras.ru/node/{$k['nid']}</a></li>";
+        }
+        ?>
     </ol>
 
 </body>
